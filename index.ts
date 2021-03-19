4dd86f495f8c1ac6129b2b9bb98e59ecae03cfe0
Andrei Vvedenskii <andrei.vvedenskii@netcracker.com>
2021-03-19 19:01:10 +0400

    update

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-let q :any = null;
+type User = {name: string, age: number, getJobs?: () => string[], jobs: [string, string]};
+
+let user:  User= {
+    name: 'andr',
+    age: 33,
+    jobs: ['a', 'b'],
+    getJobs(){
+        return this.jobs;
+    }
+};
 
 
 
+function printUser(user: User): void {
+    console.log(user.name + ' ' + user.age);
+}
+
+printUser(user);
+
+let variable;
